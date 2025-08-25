304bdbb8a10e6ace07ee96f03ea60b62d8d244cb
priyanshu31-arch <priyanshu310102@gmail.com>
2025-08-25 15:23:42 +0530

    update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config();s
 
 const authMiddleware = (req, res, next) => {
   const token = req.header('x-auth-token');
