282d7152c644a983cc3c2748406189291349ad71
priyanshu31-arch <priyanshu310102@gmail.com>
2025-08-25 15:26:05 +0530

    usercontrol

---
+++ controller/userController.js
@@ -0,0 +1,39 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+    user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ msg: 'User registered successfully' });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+    const payload = { user: { id: user.id } };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
